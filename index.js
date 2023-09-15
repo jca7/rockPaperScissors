c90440190237afadf725d7d624f4601cc14fecf0
Jesse Chan <j.chan092@gmail.com>
2023-09-15 16:54:23 -0700

    Completed project
    - cleaned up code, got rid of comments

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-// 1) Create an array to store "Rock" , "Paper" and "Scissors"
-const options = ["rock", "paper", "scissors"]
-// 2) create a function that randomly generates a number then use that to randomly select an item from the array above
 function getComputerChoice (){
+  const options = ["rock", "paper", "scissors"]
   let randomNumber = Math.floor(Math.random() * 3);
-
   let choice = options[randomNumber];
   return choice;
 }
-
-// 3) write a function that takes 2 parameters: playerSelection & computerSelection 
-// create logic on what beats what (Rock beat scissors, scissors beats paper, paper beats rock, if same = tie)
-// compare playerSelection vs. computerSelection to see who wins or if there is a tie
-// return a string that declares the winner of the round or tie 
-
-// have to find a way to keep score for both player and computer
-// need to create a container to hold the score for both  
 
 let playerScore = 0;
 let computerScore = 0;
@@ -22,53 +11,46 @@ let computerScore = 0;
 function playRound(playerSelection, computerSelection) {
   
   if (playerSelection === "rock" && computerSelection === "rock") {
-    return "Tie game";
+    console.log("Tie game this round");
   } else if (playerSelection === "rock" && computerSelection === "paper") {
-    computerScore++;
-    return "Computer wins, sorry";
+    console.log("Computer wins this round")
+    return  computerScore++;
   } else if (playerSelection === "rock" && computerSelection ==="scissors") {
-    playerScore++;
-    return "Player wins!";
+    console.log("Player wins this round");
+    return playerScore++;
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    computerScore++;
-    return "Computer wins, sorry";
+    console.log("Computer wins this round")
+    return  computerScore++;
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    playerScore++;
-    return "Player wins!";
+    console.log("Player wins this round");
+    return playerScore++;
   } else if (playerSelection === "scissors" && computerSelection === "scissors") {
-    return "Tie game";
+    console.log("Tie game this round");
   } else if (playerSelection === "paper" && computerSelection === "rock") {
-    playerScore++;
-    return "Player wins!";
+    console.log("Player wins this round");
+    return playerScore++;
   } else if (playerSelection === "paper" && computerSelection === "paper") {
-    return "Tie game";
+    console.log("Tie game this round");
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    computerScore++;
-    return "Computer wins, sorry";
+    console.log("Computer wins this round")
+    return  computerScore++;
   } else {
-    return"Sorry, that's not a valid option"
+    return "Sorry, that's not a valid option"
   }
-
 }
 
-// 4) write a function called game() put the previous function in here
-// this function should keep score and reports a winner or loser at the end. 
 function game() {
-
-  
   for (let i = 0; i < 5; i++) { 
-
     let playerSelects = prompt("Pick between rock paper or scissors").toLowerCase();
-    const computerSelects = getComputerChoice();
-
+    let computerSelects = getComputerChoice();
     playRound(playerSelects, computerSelects);
   }
   if (playerScore > computerScore) {
-    console.log("player wins!");
+    console.log("Player wins the game! By a score of " + playerScore + " to " + computerScore);
   } else if (computerScore > playerScore) {
-    console.log("computer wins!");
+    console.log("Computer wins the game! By a score of " + computerScore + " to " + playerScore);
   } else if (computerScore === playerScore) {
-    console.log("Tie");
+    console.log("No one wins this game, we ended up with a tie.");
   }
 }
 
@@ -76,11 +58,3 @@ game();
 
 
 
-
-// if player wins, then increment playerScore by 1
-// if computer wins, then increment computerScore by 1
-// if tie, then no one wins 
-// after 5 rounds of the game, announce a winner.
-// the winner should be the greater of playerScore vs. Computer 
-// if playerScore > than computerScore then print out player wins
-// if computerScore > than playerScore then print out computer wins. 

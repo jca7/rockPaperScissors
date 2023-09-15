46fab63f19b0c75a3d76ac36d2222d4a7c13d1c8
Jesse Chan <j.chan092@gmail.com>
2023-09-15 11:16:58 -0700

    Planning stage
    - Created Pseudocode as a start
    - Made sure to layout some of the steps needed to complete the project
    - Moving onto code next

---
+++ index.js
@@ -0,0 +1,11 @@
+// 1) Create an array to store "Rock" , "Paper" and "Scissors"
+
+// 2) create a function that randomly generates a number then use that to randomly select an item from the array above
+
+// 3) write a function that takes 2 parameters: playerSelection & computerSelection 
+// create logic on what beats what (Rock beat scissors, scissors beats paper, paper beats rock, if same = tie)
+// compare playerSelection vs. computerSelection to see who wins or if there is a tie
+// return a string that declares the winner of the round or tie 
+
+// 4) write a function called game() put the previous function in here
+// this function should keep score and reports a winner or loser at the end. 

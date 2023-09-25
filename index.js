f5f966d1e4b57ca5fbeae20119462e16844cb4cc
Jesse Chan <j.chan092@gmail.com>
2023-09-24 20:53:47 -0700

    Testing out DOM features + brainstorming - Not sure how to take a button click and turn it into a playerSelection - prompts were easier... trying to figure out a way to capture the mouse click selection - and turn that into a playerSelection to use for the game.

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 function getComputerChoice (){
-  const options = ["rock", "paper", "scissors"]
+  const options = ["rock", "paper", "scissors"];
   let randomNumber = Math.floor(Math.random() * options.length);
   let choice = options[randomNumber];
   return choice;
 }
-
 
 
 function playRound(playerSelection, computerSelection) {
@@ -12,13 +11,13 @@ function playRound(playerSelection, computerSelection) {
   if (playerSelection === "rock" && computerSelection === "rock") {
     console.log("Tie game this round");
   } else if (playerSelection === "rock" && computerSelection === "paper") {
-    console.log("Computer wins this round")
+    console.log("Computer wins this round");
     return  computerScore++;
   } else if (playerSelection === "rock" && computerSelection ==="scissors") {
     console.log("Player wins this round");
     return playerScore++;
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    console.log("Computer wins this round")
+    console.log("Computer wins this round");
     return  computerScore++;
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
     console.log("Player wins this round");
@@ -31,10 +30,10 @@ function playRound(playerSelection, computerSelection) {
   } else if (playerSelection === "paper" && computerSelection === "paper") {
     console.log("Tie game this round");
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    console.log("Computer wins this round")
+    console.log("Computer wins this round");
     return  computerScore++;
   } else {
-    return "Sorry, that's not a valid option"
+    return "Sorry, that's not a valid option";
   }
 }
 
@@ -66,6 +65,11 @@ buttons.forEach((button) => {
   })
 })
 
+// Does this give me back a node list?? 
+// can i then work with this like an array??
+// i.e. if playerChoice === 'rock then do something with it??
+
+
 // const rock = document.querySelector('#rock'); 
 
 // rock.addEventListener('click', (e) => {
@@ -73,7 +77,11 @@ buttons.forEach((button) => {
 //   return rock.id;
 // })
 
-
+// const rock = document.getElementById('rock');
+// console.log(rock)
+// console.log(rock)
+// console.log(rock)
+// console.log(rock)
 
 // const paper = document.querySelector('#paper'); 
 
@@ -88,3 +96,13 @@ buttons.forEach((button) => {
 //   console.log(e.target);
 //   return scissors.id;
 // })
+
+
+//GOAL OF PROJECT
+
+// allow user to select between rock, paper or scissors with a mouse click
+// once an item is selected, we want to put that selection somewhere in the game that we can use
+// once its in a place where we can use it we then need to compare it with the computer's choice. 
+// that way we can actually play the game again. 
+// After 1 single loop is done, then we ask the user to input a second selection. 
+
